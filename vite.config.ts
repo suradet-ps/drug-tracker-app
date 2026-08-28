63f43cd061ed35cf57fb06d74fdb2861c1348016
Suradet Pratomsak <pharmacistsabot@gmail.com>
2026-08-28 20:22:36 +0700

    style: restore original import order that satisfies en-locale collation

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { fileURLToPath, URL } from 'node:url';
 import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
