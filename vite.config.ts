5b72858faaeca58ae5bf4f68287ce0b1a05a90c9
Suradet Pratomsak <pharmacistsabot@gmail.com>
2026-08-31 14:25:38 +0700

    chore: update all dependencies to fix @stylistic/eslint-plugin compatibility with ESLint 10

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
